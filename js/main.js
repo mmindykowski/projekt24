bd4574d9a7a1ed76ee4b816d15b4c60a0cae37eb
Mikolaj Mindykowski <mikolaj.mindykowski@gmail.com>
2023-03-30 21:23:58 +0200

    js wieksza liczba

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+
+let num2 = 5;
+
+function wieksza (num1, num2) {
+    if ( num1 < num2) {
+        return num2;
+    } else {
+        return num1;
+    }
+}
+
+console.log(wieksza(num1, num2));
